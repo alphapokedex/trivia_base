5d9a959e248c808f8685d6a0fa9f56428ddab798
Chirag <52071686+alphapokedex@users.noreply.github.com>
2025-10-05 23:48:13 +0530

    chore: cleanup

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,7 +22,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-
-tasks.withType(JavaCompile) {
-    options.compilerArgs += ["-Xlint:deprecation"]
-}
